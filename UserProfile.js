e33504cd99b97dfd0e79c7cc830f6d4852acb092
Zaren Veto <elitearabecreativeunion2010@gmail.com>
2026-04-15 17:39:26 +0100

    Create UserProfile.js

---
+++ UserProfile.js
@@ -0,0 +1,5 @@
+      {/* الاسم مع شارة التوثيق الملكية */}
+      <View style={styles.nameRow}>
+        <Text style={styles.userName}>Princess Narzine Bani Hashem</Text>
+        <Image source={require('./verified_badge.png')} style={styles.verifiedIcon} />
+      </View>
